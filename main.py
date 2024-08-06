84c60d66ca03988b2f01c24c9679eb32af32b66a
Ayush Mukherjee <ayushmukherjee2017@gmail.com>
2024-08-06 15:55:06 +0530

    update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ filename = 'Imp.txt'
 with open(filename, 'a') as f:
     now = datetime.now()
     f.write(f'Today is {now.strftime("%Y-%m-%d")}\n')
-    f.write(f'This one is for you david\n')
+    f.write(f'Manners maketh man\n')
